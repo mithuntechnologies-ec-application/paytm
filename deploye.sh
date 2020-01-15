da62206fe2810ebc44fefad8f98120f8535b09ee
balawas <balawas500@gmail.com>
2020-01-15 07:24:52 -0800

    initial commit

---
+++ deploye.sh
@@ -0,0 +1,8 @@
+Xhai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
