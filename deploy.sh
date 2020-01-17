cc43894940609a2c4dc95690be62575ed337a588
balawas <balawas500@gmail.com>
2020-01-17 03:11:22 -0800

    Revert "new file was created"
    This reverts commit c5a6bafcba57707e4ad55c56527b2173b04d8cf7.

--- deploy.sh
+++
@@ -1,9 +0,0 @@
- hai hello am updating now 
-Xhai i have written frist script in linux
-hai i have written frist script in linux
-hai i have written frist script in linux
-hai i have written frist script in linux
-hai i have written frist script in linux
-hai i have written frist script in linux
-hai i have written frist script in linux
-hai i have written frist script in linux
