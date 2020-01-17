c5a6bafcba57707e4ad55c56527b2173b04d8cf7
balawas <balawas500@gmail.com>
2020-01-17 02:54:00 -0800

    new file was created

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+ hai hello am updating now 
+Xhai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
+hai i have written frist script in linux
